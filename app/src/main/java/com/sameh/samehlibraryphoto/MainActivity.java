27b36f4b71c33c62008d1b5ef084bedab39ae345
SamehZizo <31700862+SamehZizo@users.noreply.github.com>
2018-08-03 16:27:35 +0200

    1.0.0

--- app/src/main/java/com/sameh/samehlibraryphoto/MainActivity.java
+++ app/src/main/java/com/sameh/samehlibraryphoto/MainActivity.java
@@ -10,55 +10,11 @@ import android.widget.ImageView;
 import com.sameh.photo.camera.Camera;
 import com.sameh.photo.gallery.Gallery;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-
-    ImageView image;
-
-    Camera camera;
-    Gallery gallery;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        image = findViewById(R.id.image);
-        image.setOnClickListener(this);
-
-        camera = new Camera(this);
-        gallery = new Gallery(this);
-    }
-
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.image :
-                camera.takePhoto();
-                //gallery.chooseImage();
-                break;
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        camera.onRequestCameraPermission(requestCode, permissions, grantResults);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        camera.onActivityCameraResult(requestCode, resultCode, new Camera.Completed() {
-            @Override
-            public void onCompleted(Bitmap bitmap, Uri imageUri) {
-                image.setImageBitmap(bitmap);
-            }
-        });
-        gallery.onActivityGalleryResult(requestCode, resultCode, data, new Gallery.Completed() {
-            @Override
-            public void onCompleted(Bitmap bitmap, Uri imageUri) {
-                image.setImageBitmap(bitmap);
-            }
-        });
     }
 }
