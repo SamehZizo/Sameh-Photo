1183175c3735a252f58e1688771731c94eec4b22
SamehZizo <31700862+SamehZizo@users.noreply.github.com>
2018-08-05 14:59:42 +0200

    1.0.1

--- app/src/main/java/com/sameh/samehlibraryphoto/MainActivity.java
+++ app/src/main/java/com/sameh/samehlibraryphoto/MainActivity.java
@@ -1,14 +1,7 @@
 package com.sameh.samehlibraryphoto;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.ImageView;
-import com.sameh.photo.camera.Camera;
-import com.sameh.photo.gallery.Gallery;
 
 public class MainActivity extends AppCompatActivity {
 
